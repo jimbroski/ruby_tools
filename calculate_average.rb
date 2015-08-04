0d2ba8b59a7fffcd4e1d974e0633797e9b695e43
jimbroski <sam.bogu@gmail.com>
2015-08-04 15:55:47 +0200

    average calculator now has the option to stay on

--- calculate_average.rb
+++ calculate_average.rb
@@ -1,16 +1,22 @@
 puts "Calculate the average of your school grades"
 
-result = 0.00
-total_grades = 0
+run = "y"
 
-(1..6).each do |n|
-  puts "Amount of #{n}s:"
-  amount = gets.chomp.to_i
+while (run == "y")
+  result = 0.00
+  total_grades = 0
 
-  result = result + (amount * n)
-  total_grades = total_grades + amount
+  (1..6).each do |n|
+    puts "Amount of #{n}s:"
+    amount = gets.chomp.to_i
+
+    result = result + (amount * n)
+    total_grades = total_grades + amount
+  end
+
+  result = result / total_grades
+
+  puts "Your average is: #{result}"
+  puts "Do you want to calculate more? (y/n)"
+  run = gets.chomp
 end
-
-result = result / total_grades
-
-puts "Your average is: #{result}"
